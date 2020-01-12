bb59e36bfb67be4663c294008864817e5d6dda9f
Zainab Ghori <47547647+zghori8@users.noreply.github.com>
2020-01-12 00:32:44 -0500

    Add files via upload

---
+++ A1/src/Helper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "Helper.h"
+#include "SVGParser.h"
+#include "LinkedListAPI.h"
+
+// SVGimage * new_svgimage()
+// {
+//     SVGimage *image = malloc(sizeof(SVGimage));
+
+//     char * (*printCircle)(void *) = &circleToString;
+
+//     image->circles = initializeList(printCircle, )
+// }
+
+// char* createNewString(void)
+// {
+// 	char* newString = calloc(50, sizeof(char));
+	
+// 	return newString;
+// }
